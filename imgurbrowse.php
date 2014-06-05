a1341224bb258a48bc34952a0c36341073a06583
Joseph Thomas <greywolf921@hotmail.com>
2014-06-05 01:42:37 -0600

    Update imgurbrowse.php

--- imgurbrowse.php
+++ imgurbrowse.php
@@ -21,6 +21,16 @@
 	// If size for the thumbnails is specified then use it, otherwise default to 150px
 	$size = isset($_GET['size']) ? $_GET['size'] : 150;
 	
+	/*	Check for Mode setting in URL string, 
+		if it is specified then use it otherwise default to "fast".
+		fast mode = will not check to see if image exists. 
+		This mode is much faster but will display many of the imgur error images.
+		slow mode = this mode will only display actual images but is much slower 
+		as it verifies each image first.
+	*/
+	$mode = isset($_GET['mode']) ? $_GET['mode'] : "fast";
+	echo "<!-- mode: $mode -->";
+	
 	//  echo out for debugging purposes
 	echo "\n<!-- Values: \n SC1: $sc1 \n SC2: $sc2 \n SC3: $sc3 \n SC4: $sc4 \n SC5: $sc5 \n SIZE: $size -->";
 	
@@ -38,7 +48,7 @@
 	echo "\n<!-- Values: \n SC1: $sc1 \n SC2: $sc2 \n SC3: $sc3 \n SC4: $sc4 \n SC5: $sc5 \n SIZE: $size -->";
 	
 	//  Function to generate the image name and echo it out
-	function genimgname($n1,$n2,$n3,$n4,$n5,$size,$characters){
+	function genimgname($n1,$n2,$n3,$n4,$n5,$size,$characters,$mode){
 		
 		//  Build image name string from characters string location
 		$imgname = substr($characters,$n1,1);
@@ -47,25 +57,38 @@
 		$imgname .= substr($characters,$n4,1);
 		$imgname .= substr($characters,$n5,1);
 	
-	  //  echo out for debugging purposes
+		//  echo out for debugging purposes
 		echo "$imgname.jpg -->\n";
 		
-		//  echo out image name
-		echo "<a href='http://i.imgur.com/$imgname.jpg' target='_blank'><img src='http://i.imgur.com/$imgname.jpg' border='0' height='$size' width='$size'></a>";
+		$imgpath1 = "http://i.imgur.com/" . $imgname . ".jpg";
 		
+		//	Check which mode we are using
+		switch($mode){
+			case "slow":
+				$headers = get_headers($imgpath1);
+				echo "<!-- $mode : ".$imgpath1." ".$headers[0]." -->\n";
+				if ($headers[0] == "HTTP/1.1 200 OK"){ // If image exists...
+					echo "<a href='$imgpath1' target='_blank'><img src='$imgpath1' border='0' height='$size' width='$size'></a>";
+				}
+				break;
+			default:
+				echo "<!-- $mode : ".$imgpath1." -->\n";
+				echo "<a href='$imgpath1' target='_blank'><img src='$imgpath1' border='0' height='$size' width='$size'></a>";
+				break;
+		}	
 	}
 	
 	//  Loop through entire characters string for the 5th character of the image name
 	for($sc5 = 0; $sc5 <= (strlen($characters) - 1); $sc5++){
 	  //echo out for debugging purposes
-		echo "\n<!-- genimagename($sc1,$sc2,$sc3,$sc4,$sc5,$size,$characters) = ";
-		genimgname($sc1,$sc2,$sc3,$sc4,$sc5,$size,$characters);
+		echo "\n<!-- genimagename($sc1,$sc2,$sc3,$sc4,$sc5,$size,$characters,$mode) = ";
+		genimgname($sc1,$sc2,$sc3,$sc4,$sc5,$size,$characters,$mode);
 	}
 	
 	//  increase 4th character of the image name location
 	$sc4++;
 	
 	//  Link to next page
-	echo "<a href='imgur_gendb.php?location=$sc1,$sc2,$sc3,$sc4&size=$size'><img src='http://i.imgur.com/kstqS.png' border='0' width='$size' height='$size'></a>";
+	echo "<a href='".$PHP_SELF."?location=$sc1,$sc2,$sc3,$sc4&size=$size&mode=$mode'><img src='http://i.imgur.com/kstqS.png' border='0' width='$size' height='$size'></a>";
 	
 ?>
